8d93bd78007e44a040c9edcac1f50502cbcdcad6
Tor <findtor@gmail.com>
2014-08-04 18:22:37 +0200

    Added a button "Add new session" to the mainlayout

--- app/build/generated/source/r/debug/com/example/torgammelgard/pokerhourly/R.java
+++ app/build/generated/source/r/debug/com/example/torgammelgard/pokerhourly/R.java
@@ -18,7 +18,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int add_new_session=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -28,8 +29,8 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int addButton=0x7f050001;
+        public static final int app_name=0x7f050002;
     }
     public static final class style {
         /**  Customize your theme here. 
