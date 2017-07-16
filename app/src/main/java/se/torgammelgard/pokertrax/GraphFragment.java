cf0ac6242f5f999315f0b1926366b91ca2ef44d0
Tor Gammelgård <tor.gammelgard@gmail.com>
2017-07-16 18:36:14 +0200

    Removed unused folders/files. Tried to fix gradle and dependencies.

--- app/src/main/java/se/torgammelgard/pokertrax/GraphFragment.java
+++ app/src/main/java/se/torgammelgard/pokertrax/GraphFragment.java
@@ -1,20 +1,18 @@
 package se.torgammelgard.pokertrax;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.GraphViewStyle;
-import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 
 public class GraphFragment extends android.support.v4.app.Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.graph_frag, container, false);
@@ -28,46 +26,39 @@ public class GraphFragment extends android.support.v4.app.Fragment {
     }
 
     private void updateGraph() {
-        /* Prepare the graphView data*/
-        ArrayList<Session> sessionList = ((MainApp)getActivity().getApplication())
+
+        // get the graph view
+        if (getView() == null) {
+            return;
+        }
+        GraphView graph = (GraphView) getView().findViewById(R.id.graph);
+
+        // get and construct the data
+        ArrayList<Session> sessionList = ((MainApp) getActivity().getApplication())
                 .mDataSource.getLastSessions(20);
-        int[] resultIntArray = new int[sessionList.size() + 1];
-        int index = 0;
-        resultIntArray[index++] = 0;
+        int[] session_results = new int[sessionList.size() + 1];
+
+        DataPoint[] dataPoints = new DataPoint[session_results.length];
+
+        // start data at 0, 0
+        dataPoints[0] = new DataPoint(0.0, 0.0);
+
+        int index = 1;
+
+        // add the rest of the results
         for (Session session : sessionList) {
-            resultIntArray[index++] = session.getResult();
+            dataPoints[index] = new DataPoint(index, session.getResult() / 100);
+            index++;
         }
-        GraphView.GraphViewData[] data = makeData(resultIntArray);
 
-        /* style - nested class*/
-        GraphViewSeries.GraphViewSeriesStyle g =
-                new GraphViewSeries.GraphViewSeriesStyle(Color.GREEN, 7);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
 
-        /* init the graph */
-        GraphViewSeries graphViewSeries = new GraphViewSeries("graphViewSeries", g, data);
-        GraphView graphView = new LineGraphView(getActivity(), getString(R.string.graph_title));
+        // style graph
 
-        /* Style the graph */
-        GraphViewStyle style = new GraphViewStyle();
-        style.setNumHorizontalLabels(11);
-        graphView.setGraphViewStyle(style);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0.0);
+        graph.getViewport().setMaxX(dataPoints.length - 0.5);
 
-        /* add the data and show*/
-        graphView.addSeries(graphViewSeries);
-        if (getView() != null) {
-            LinearLayout layout = (LinearLayout) getView().findViewById(R.id.graphLayout);
-            layout.addView(graphView);
-        }
-    }
-
-    /** returns data for the GraphView */
-    private GraphView.GraphViewData[] makeData(int[] dataList) {
-        double sum = 0;
-        GraphView.GraphViewData[] data = new GraphView.GraphViewData[dataList.length];
-        for (int index = 0; index < data.length; index++) {
-            sum += (double)dataList[index] / 100;
-            data[index] = new GraphView.GraphViewData(index, sum);
-        }
-        return data;
+        graph.addSeries(series);
     }
 }
