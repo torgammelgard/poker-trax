57a5cd58a60d47ffe45d9dd8ea5a67e40abf6281
Tor Gammelgård <tor.gammelgard@gmail.com>
2018-06-20 21:57:35 +0200

    Replace Tab activity with MainActivity

--- app/src/main/kotlin/se/torgammelgard/pokertrax/activities/MainActivity.kt
+++ app/src/main/kotlin/se/torgammelgard/pokertrax/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import se.torgammelgard.pokertrax.R
 
